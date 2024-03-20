6812c86226432489f575345e20236cc8a757f88c
FirePh0enix <leonard@delbecq.net>
2024-03-20 23:05:46 +0100

    Fix ft_vector_size causing Segfaults

--- ft_vector_utils.c
+++ ft_vector_utils.c
@@ -6,7 +6,7 @@
 /*   By: ledelbec <ledelbec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 23:23:51 by ledelbec          #+#    #+#             */
-/*   Updated: 2024/02/07 19:21:15 by ledelbec         ###   ########.fr       */
+/*   Updated: 2024/03/20 22:57:37 by ledelbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_vector_size(void *vec)
 {
-	return (((t_vecdata *)vec)->size);
+	return (((t_vecdata *)(vec - sizeof(t_vecdata)))->size);
 }
 
 void	ft_vector_free(void *vec)
