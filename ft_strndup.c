6e700baa22460ee6709e573a7ca22309469f64d4
FirePh0enix <leonard@delbecq.net>
2024-04-21 13:28:59 +0200

    Fix ft_strndup

--- ft_strndup.c
+++ ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: ledelbec <ledelbec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:01:16 by ledelbec          #+#    #+#             */
-/*   Updated: 2024/04/19 14:02:57 by ledelbec         ###   ########.fr       */
+/*   Updated: 2024/04/20 16:26:47 by ledelbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strndup(const char *s, size_t n)
 	size = ft_strlen(s);
 	if (size > n)
 		size = n;
-	dup = malloc(ft_strlen(s) + 1);
+	dup = malloc(n + 1);
 	if (!dup)
 		return (NULL);
 	ft_memcpy(dup, s, n);
