2b0d9b2287a843f797c95c9f00bf8b6151771de8
FirePh0enix <leonard@delbecq.net>
2024-04-24 16:52:13 +0200

    Protect string lib

--- string.c
+++ string.c
@@ -6,7 +6,7 @@
 /*   By: ledelbec <ledelbec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 11:51:47 by ledelbec          #+#    #+#             */
-/*   Updated: 2024/04/09 12:12:28 by ledelbec         ###   ########.fr       */
+/*   Updated: 2024/04/24 16:51:35 by ledelbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ void	*str_append(t_str *str, char *s)
 {
 	const size_t	size = ft_strlen(s);
 
+	if (!str->data)
+		return (NULL);
 	if (str->size + size + 1 > str->capacity)
 	{
 		str->data = ft_realloc(str->data, str->capacity, str->capacity + size);
@@ -44,6 +46,8 @@ void	*str_append_n(t_str *str, char *s, size_t n)
 {
 	const size_t	size = n;
 
+	if (!str->data)
+		return (NULL);
 	if (str->size + size + 1 > str->capacity)
 	{
 		str->data = ft_realloc(str->data, str->capacity, str->capacity + size);
